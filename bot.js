1c6ca968c9e5d612eba0c23f9bb81e90506179d6
SarvarbekMuminjonov <hechkimhacker@gmail.com>
2020-06-24 12:48:06 +0500

    t

--- bot.js
+++ bot.js
@@ -16,7 +16,8 @@ Telegraf JS - Uzbekistan guruhiga xush kelibsiz.
 bot.telegram.setWebhook(`${URL}/bot${TOKEN}`);
 app.use(bot.webhookCallback(`/bot${TOKEN}`));
 
-bot.on('new_chat_members',(ctx,next)=>{
+bot.on('new_chat_members',(ctx)=>{
+    ctx.deleteMessage()
     ctx.deleteMessage()
     bot.telegram.sendMessage(ctx.chat.id,
         `Assalomu alaykum ` + `<a href="tg://user?id=${ctx.update.message.new_chat_member.id}">${ctx.update.message.new_chat_member.first_name}</a>`
@@ -24,9 +25,9 @@ bot.on('new_chat_members',(ctx,next)=>{
     ` <a href="https://t.me/telegraf_uzb/31">haqida </a>` +` tanishib chiqishingizni maslahat beramiz`,
     {parse_mode:'HTML',disable_web_page_preview:true}
     )
-    next()
+ 
 })
-bot.on('text', (ctx,next) => {
+bot.on('text', (ctx) => {
     if (ctx.message.text.slice(0, 3) == '#js') {
       let url = "https://rextester.com/rundotnet/api"
       let form = { "LanguageChoice": 23, "Program": ctx.message.text.slice(4) }
@@ -40,7 +41,7 @@ bot.on('text', (ctx,next) => {
         }
       })
     }
-    next()
+    
   })
 
 app.listen(PORT , () => {
