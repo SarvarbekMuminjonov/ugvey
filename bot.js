e411a8a01c2e435fb176d6a2c959aa962b32062b
Sarvarbek Muminjonov <smomimjonov20@gmail.com>
2021-01-29 18:40:13 +0500

    make it better

--- bot.js
+++ bot.js
@@ -38,6 +38,9 @@ bot.on('new_chat_members',(ctx)=>{
         if (data.Errors) {
           bot.telegram.sendMessage(ctx.chat.id,`<code>${escapeOutOfRange(data.Errors)}</code>`,
           {reply_to_message_id:ctx.message.message_id,parse_mode:'HTML'})
+          console.log(
+            data.Errors
+          )
          // ctx.replyWithHTML(`<code>${escapeOutOfRange(data.Errors)}</code>`, { reply_to_message_id: ctx.message.message_id })
         } else {
           bot.telegram.sendMessage(ctx.chat.id,escapeOutOfRange(data.Result),{reply_to_message_id:ctx.message.message_id})
